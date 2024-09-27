85e1a899c43b4eef73832cbb2fbe01ab31c1bb07
SandisoDlamini <85376727+SandisoDlamini@users.noreply.github.com>
2024-09-27 17:38:12 +0200

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+const dialog = document.getElementById("loginDialog");
+const showButton = document.getElementById("showLogin");
+const closeButton = document.querySelector("dialog button");
+
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
